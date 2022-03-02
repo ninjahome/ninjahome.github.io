4591bcffdea607a382e4dd6453a09dae83f2548e
hopwesley <ws@sinoc.io>
2022-03-02 15:33:39 +0800

    version refactor

--- main.go
+++ main.go
@@ -3,15 +3,20 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 )
+
+const VersionSplit = "=android_apk_version="
 
 type VersionDes struct {
 	Ver int
+	Url string
 	Des []string
 }
 
 var curVer = &VersionDes{
 	Ver: 30201,
+	Url: "https://dl.testfairy.com/download/74W3CDHG60RJTC1D82NKRPC60FPAFHT8N9AYAE2D87ZMWQ40/ninja_3.2.00-testfairy.apk",
 	Des: []string{
 		"1.增加转账功能",
 		"2.增加名片功能",
@@ -21,4 +26,7 @@ var curVer = &VersionDes{
 func main() {
 	result, _ := json.Marshal(curVer)
 	fmt.Println(string(result))
+
+	str := fmt.Sprintf("%s%s%s", VersionSplit, result, VersionSplit)
+	_ = ioutil.WriteFile("version.md", []byte(str), 0644)
 }
