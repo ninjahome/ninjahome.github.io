0bf2eb517ecee9f21d96752d1d82fda29f62b0f5
alen-x <66928150+alen-x@users.noreply.github.com>
2022-03-07 19:23:57 +0800

    Update version.js

--- version.js
+++ version.js
@@ -1,1 +1,1 @@
-{"Ver":30202,"VerName":"3.2.02","Url":"https://ninjahome.github.io/ninja.apk","Des":["1.优化消息处理流程","2.增加名片功能"]}
+{"Ver":30204,"VerName":"3.2.04","Url":"https://ninjahome.github.io/ninja.apk","Des":["1.优化消息处理流程","2.优化用户体验"]}
