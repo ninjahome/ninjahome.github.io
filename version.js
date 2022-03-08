ca81031d4b9c2506865c7ef610188eb9b689897d
hopwesley <ws@sinoc.io>
2022-03-08 22:27:55 +0800

    version update

--- version.js
+++ version.js
@@ -1,1 +1,1 @@
-{"Ver":30205,"VerName":"3.2.05","Url":"https://ninjahome.github.io/ninja.apk","Des":["1.优化用户体验"]}
+{"Ver":30206,"VerName":"3.2.06","Url":"https://ninjahome.github.io/ninja.apk","Des":["1.优化用户体验"]}
