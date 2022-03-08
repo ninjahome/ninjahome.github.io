7780778101bc145f3b7b3dd710519f552b567517
alen-B <alenworkworld@gmail.com>
2022-03-08 16:28:44 +0800

    update version.js

--- version.js
+++ version.js
@@ -1,1 +1,1 @@
-{"Ver":30204,"VerName":"3.2.04","Url":"https://ninjahome.github.io/ninja.apk","Des":["1.优化消息处理流程","2.优化用户体验"]}
+{"Ver":30205,"VerName":"3.2.05","Url":"https://ninjahome.github.io/ninja.apk","Des":["1.优化用户体验"]}
