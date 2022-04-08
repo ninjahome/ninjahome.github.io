d89cffabdfb44d982c902feb8d0b806ddd6c3030
alen-x <66928150+alen-x@users.noreply.github.com>
2022-04-08 22:20:17 +0800

    Update version.js

--- version.js
+++ version.js
@@ -1,1 +1,1 @@
-{"Ver":30206,"VerName":"3.2.06","Url":"https://ninjahome.github.io/ninja.apk","Des":["1.优化用户体验","2.增加手势解锁功能"]}
+{"Ver":30208,"VerName":"3.2.08","Url":"https://ninjahome.github.io/ninja.apk","Des":["1.优化用户体验","2.增加手势解锁功能"]}
