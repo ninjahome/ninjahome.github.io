578eaa974ba431fbd72cc4b6bfe42d8d7fc52374
alen-x <alen-x@tutanota.com>
2023-02-22 09:03:54 +0800

    update version.js

--- version.js
+++ version.js
@@ -1,1 +1,1 @@
-{"Ver":30210,"VerName":"3.2.10","Url":"https://ninjahome.github.io/ninja.apk","Des":["1.优化用户体验"]}
+{"Ver":30301,"VerName":"3.3.01","Url":"https://ninjahome.github.io/ninja.apk","Des":["1.Damus账号互通"]}
