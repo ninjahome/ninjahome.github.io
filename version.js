1afe1fde9d56205580cc7d23c54de762a8bec77b
alen-x <alen-x@tutanota.com>
2023-05-22 08:00:07 +0800

    update version 3.3.8

--- version.js
+++ version.js
@@ -1,1 +1,1 @@
-{"Ver":30306,"VerName":"3.3.6","Url":"https://ninjahome.github.io/ninja.apk","Des":["1.优化app"]}
+{"Ver":30308,"VerName":"3.3.8","Url":"https://ninjahome.github.io/ninja.apk","Des":["1.优化app"]}
